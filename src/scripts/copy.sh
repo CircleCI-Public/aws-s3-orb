d04ed76d669d6346688ea617e86777aed38d92c6
Brian Vu <emailbrian.vu@gmail.com>
2023-03-16 17:06:02 -0700

    fix: remove quotes from variable in copy command

--- src/scripts/copy.sh
+++ src/scripts/copy.sh
@@ -3,8 +3,9 @@ PARAM_AWS_S3_FROM=$(eval echo "${PARAM_AWS_S3_FROM}")
 PARAM_AWS_S3_TO=$(eval echo "${PARAM_AWS_S3_TO}")
 PARAM_AWS_S3_ARGUMENTS=$(eval echo "${PARAM_AWS_S3_ARGUMENTS}")
 
+# shellcheck disable=SC2086
 if [ -n "${PARAM_AWS_S3_ARGUMENTS}" ]; then
-    set -- "$@" "${PARAM_AWS_S3_ARGUMENTS}"
+    set -- "$@" ${PARAM_AWS_S3_ARGUMENTS}
 fi
 
 if [ -n "${PARAM_AWS_S3_PROFILE_NAME}" ]; then
