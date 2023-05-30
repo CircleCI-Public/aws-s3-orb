2e3ff942f86f24e930e6c4ec984a639b38139442
Brian Vu <emailbrian.vu@gmail.com>
2023-05-30 15:04:35 -0700

    refactor: replace eval echo

--- src/scripts/copy.sh
+++ src/scripts/copy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-ORB_EVAL_FROM=$(eval echo "${ORB_EVAL_FROM}")
-ORB_EVAL_TO=$(eval echo "${ORB_EVAL_TO}")
-ORB_EVAL_ARGUMENTS=$(eval echo "${ORB_EVAL_ARGUMENTS}")
+ORB_EVAL_FROM=$(circleci env subst "${ORB_EVAL_FROM}")
+ORB_EVAL_TO=$(circleci env subst "${ORB_EVAL_TO}")
+ORB_EVAL_ARGUMENTS=$(circleci env subst "${ORB_EVAL_ARGUMENTS}")
 
 if [ -n "${ORB_EVAL_ARGUMENTS}" ]; then
     set -- "$@" "${ORB_EVAL_ARGUMENTS}"
--- src/scripts/sync.sh
+++ src/scripts/sync.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-ORB_EVAL_FROM=$(eval echo "${ORB_EVAL_FROM}")
-ORB_EVAL_TO=$(eval echo "${ORB_EVAL_TO}")
-ORB_EVAL_ARGUMENTS=$(eval echo "${ORB_EVAL_ARGUMENTS}")
+set -x
+ORB_EVAL_FROM=$(circleci env subst "${ORB_EVAL_FROM}")
+ORB_EVAL_TO=$(circleci env subst "${ORB_EVAL_TO}")
+ORB_EVAL_ARGUMENTS=$(circleci env subst "${ORB_EVAL_ARGUMENTS}")
 
 if [ -n "${ORB_EVAL_ARGUMENTS}" ]; then
     set -- "$@" "${ORB_EVAL_ARGUMENTS}"
 fi
 
-set -x
 aws s3 sync "${ORB_EVAL_FROM}" "${ORB_EVAL_TO}" --profile "${ORB_VAL_PROFILE_NAME}" "$@"
 set +x
