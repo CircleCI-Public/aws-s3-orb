51bcd7cfd50c6c66682bf5854c53c255052a28f3
Brian Vu <emailbrian.vu@gmail.com>
2023-05-30 15:01:50 -0700

    refactor: rename varaibles to orb standards

--- src/scripts/sync.sh
+++ src/scripts/sync.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-PARAM_AWS_S3_FROM=$(eval echo "${PARAM_AWS_S3_FROM}")
-PARAM_AWS_S3_TO=$(eval echo "${PARAM_AWS_S3_TO}")
-PARAM_AWS_S3_ARGUMENTS=$(eval echo "${PARAM_AWS_S3_ARGUMENTS}")
+ORB_EVAL_FROM=$(eval echo "${ORB_EVAL_FROM}")
+ORB_EVAL_TO=$(eval echo "${ORB_EVAL_TO}")
+ORB_EVAL_ARGUMENTS=$(eval echo "${ORB_EVAL_ARGUMENTS}")
 
-if [ -n "${PARAM_AWS_S3_ARGUMENTS}" ]; then
-    set -- "$@" "${PARAM_AWS_S3_ARGUMENTS}"
+if [ -n "${ORB_EVAL_ARGUMENTS}" ]; then
+    set -- "$@" "${ORB_EVAL_ARGUMENTS}"
 fi
 
 set -x
-aws s3 sync "${PARAM_AWS_S3_FROM}" "${PARAM_AWS_S3_TO}" --profile "${ORB_VAL_S3_PROFILE_NAME}" "$@"
+aws s3 sync "${ORB_EVAL_FROM}" "${ORB_EVAL_TO}" --profile "${ORB_VAL_PROFILE_NAME}" "$@"
 set +x
