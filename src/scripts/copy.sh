eea046ab690144df8ce3b9dea6107f233bc944f1
Brian Vu <emailbrian.vu@gmail.com>
2023-05-31 11:03:44 -0700

    chore: add env subst to profile name

--- src/scripts/copy.sh
+++ src/scripts/copy.sh
@@ -2,11 +2,12 @@
 ORB_EVAL_FROM=$(circleci env subst "${ORB_EVAL_FROM}")
 ORB_EVAL_TO=$(circleci env subst "${ORB_EVAL_TO}")
 ORB_EVAL_ARGUMENTS=$(circleci env subst "${ORB_EVAL_ARGUMENTS}")
+ORB_EVAL_PROFILE_NAME=$(circleci env subst "${ORB_EVAL_PROFILE_NAME}")
 
 if [ -n "${ORB_EVAL_ARGUMENTS}" ]; then
     set -- "$@" "${ORB_EVAL_ARGUMENTS}"
 fi
 
 set -x
-aws s3 cp "${ORB_EVAL_FROM}" "${ORB_EVAL_TO}" --profile "${ORB_VAL_PROFILE_NAME}" "$@"
+aws s3 cp "${ORB_EVAL_FROM}" "${ORB_EVAL_TO}" --profile "${ORB_EVAL_PROFILE_NAME}" "$@"
 set +x
--- src/scripts/sync.sh
+++ src/scripts/sync.sh
@@ -3,10 +3,11 @@ set -x
 ORB_EVAL_FROM=$(circleci env subst "${ORB_EVAL_FROM}")
 ORB_EVAL_TO=$(circleci env subst "${ORB_EVAL_TO}")
 ORB_EVAL_ARGUMENTS=$(circleci env subst "${ORB_EVAL_ARGUMENTS}")
+ORB_EVAL_PROFILE_NAME=$(circleci env subst "${ORB_EVAL_PROFILE_NAME}")
 
 if [ -n "${ORB_EVAL_ARGUMENTS}" ]; then
     set -- "$@" "${ORB_EVAL_ARGUMENTS}"
 fi
 
-aws s3 sync "${ORB_EVAL_FROM}" "${ORB_EVAL_TO}" --profile "${ORB_VAL_PROFILE_NAME}" "$@"
+aws s3 sync "${ORB_EVAL_FROM}" "${ORB_EVAL_TO}" --profile "${ORB_EVAL_PROFILE_NAME}" "$@"
 set +x
