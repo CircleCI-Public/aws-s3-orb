2fe411f205bcf2487753914dc1b2869ce4016805
Brian Vu <emailbrian.vu@gmail.com>
2023-06-12 22:18:12 -0700

    refactor: replace job image with default executor

--- src/scripts/copy.sh
+++ src/scripts/copy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-ORB_EVAL_FROM=$(eval echo "${ORB_EVAL_FROM}")
-ORB_EVAL_TO=$(eval echo "${ORB_EVAL_TO}")
-ORB_STR_ARGUMENTS=$(circleci env subst "${ORB_STR_ARGUMENTS}")
-ORB_STR_PROFILE_NAME=$(circleci env subst "${ORB_STR_PROFILE_NAME}")
+ORB_EVAL_FROM="$(eval echo "${ORB_EVAL_FROM}")"
+ORB_EVAL_TO="$(eval echo "${ORB_EVAL_TO}")"
+ORB_STR_ARGUMENTS="$(echo "${ORB_STR_ARGUMENTS}" | circleci env subst)"
+ORB_STR_PROFILE_NAME="$(circleci env subst "${ORB_STR_PROFILE_NAME}")"
 
 if [ -n "${ORB_STR_ARGUMENTS}" ]; then
     set -- "$@" "${ORB_STR_ARGUMENTS}"
--- src/scripts/sync.sh
+++ src/scripts/sync.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -x
-ORB_EVAL_FROM=$(circleci env subst "${ORB_EVAL_FROM}")
-ORB_EVAL_TO=$(circleci env subst "${ORB_EVAL_TO}")
-ORB_STR_ARGUMENTS=$(circleci env subst "${ORB_STR_ARGUMENTS}")
-ORB_STR_PROFILE_NAME=$(circleci env subst "${ORB_STR_PROFILE_NAME}")
+ORB_EVAL_FROM="$(circleci env subst "${ORB_EVAL_FROM}")"
+ORB_EVAL_TO="$(circleci env subst "${ORB_EVAL_TO}")"
+ORB_STR_ARGUMENTS="$(echo "${ORB_STR_ARGUMENTS}" | circleci env subst)"
+ORB_STR_PROFILE_NAME="$(circleci env subst "${ORB_STR_PROFILE_NAME}")"
 
 if [ -n "${ORB_STR_ARGUMENTS}" ]; then
     set -- "$@" "${ORB_STR_ARGUMENTS}"
