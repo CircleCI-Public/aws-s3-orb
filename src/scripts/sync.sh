c2e86263cccc4793fd07a5d18169b75eefe6b8b1
Brian Vu <emailbrian.vu@gmail.com>
2023-06-12 22:16:01 -0700

    chore: update varaible names

--- src/scripts/sync.sh
+++ src/scripts/sync.sh
@@ -2,12 +2,12 @@
 set -x
 ORB_EVAL_FROM=$(circleci env subst "${ORB_EVAL_FROM}")
 ORB_EVAL_TO=$(circleci env subst "${ORB_EVAL_TO}")
-ORB_EVAL_ARGUMENTS=$(circleci env subst "${ORB_EVAL_ARGUMENTS}")
-ORB_EVAL_PROFILE_NAME=$(circleci env subst "${ORB_EVAL_PROFILE_NAME}")
+ORB_STR_ARGUMENTS=$(circleci env subst "${ORB_STR_ARGUMENTS}")
+ORB_STR_PROFILE_NAME=$(circleci env subst "${ORB_STR_PROFILE_NAME}")
 
-if [ -n "${ORB_EVAL_ARGUMENTS}" ]; then
-    set -- "$@" "${ORB_EVAL_ARGUMENTS}"
+if [ -n "${ORB_STR_ARGUMENTS}" ]; then
+    set -- "$@" "${ORB_STR_ARGUMENTS}"
 fi
 
-aws s3 sync "${ORB_EVAL_FROM}" "${ORB_EVAL_TO}" --profile "${ORB_EVAL_PROFILE_NAME}" "$@"
+aws s3 sync "${ORB_EVAL_FROM}" "${ORB_EVAL_TO}" --profile "${ORB_STR_PROFILE_NAME}" "$@"
 set +x
