e73af82ed7893f063d58dfe5da32141422593a89
Brian Vu <emailbrian.vu@gmail.com>
2023-06-15 15:31:49 -0700

    fix: add posix compliance script

--- src/scripts/copy.sh
+++ src/scripts/copy.sh
@@ -1,22 +1,17 @@
-#!/bin/bash
+#!/bin/sh
 ORB_EVAL_FROM=$(circleci env subst "${ORB_EVAL_FROM}")
 ORB_EVAL_TO=$(circleci env subst "${ORB_EVAL_TO}")
 ORB_EVAL_ARGUMENTS=$(echo "${ORB_EVAL_ARGUMENTS}" | circleci env subst)
-ORB_EVAL_ARGUMENTS=${ORB_EVAL_ARGUMENTS//\'/}
 ORB_EVAL_PROFILE_NAME=$(circleci env subst "${ORB_EVAL_PROFILE_NAME}")
 
+if [ -n "${ORB_EVAL_ARGUMENTS}" ]; then
+    IFS=' '
+    set --
+    for arg in ${ORB_EVAL_ARGUMENTS}; do
+        set -- "$@" "${arg}"
+    done
+fi
 
-
-# if [ -n "${ORB_EVAL_ARGUMENTS}" ]; then
-#     aws s3 sync "${ORB_EVAL_FROM}" "${ORB_EVAL_TO}" --profile "${ORB_EVAL_PROFILE_NAME}" "${ORB_EVAL_ARGUMENTS}"
-#     # set -- "$@" "${ORB_EVAL_ARGUMENTS}"
-#     else
-#     aws s3 sync "${ORB_EVAL_FROM}" "${ORB_EVAL_TO}" --profile "${ORB_EVAL_PROFILE_NAME}"
-# fi
-
-# set -x
-# aws s3 cp "${ORB_EVAL_FROM}" "${ORB_EVAL_TO}" --profile "${ORB_EVAL_PROFILE_NAME}" "$@"
-
-
-aws s3 sync "${ORB_EVAL_FROM}" "${ORB_EVAL_TO}" --profile "${ORB_EVAL_PROFILE_NAME}" "${ORB_EVAL_ARGUMENTS:+$ORB_EVAL_ARGUMENTS}"
+set -x
+aws s3 sync "${ORB_EVAL_FROM}" "${ORB_EVAL_TO}" --profile "${ORB_EVAL_PROFILE_NAME}" "$@"
 set +x
--- src/scripts/sync.sh
+++ src/scripts/sync.sh
@@ -5,22 +5,13 @@ ORB_EVAL_TO=$(circleci env subst "${ORB_EVAL_TO}")
 ORB_EVAL_ARGUMENTS=$(echo "${ORB_EVAL_ARGUMENTS}" | circleci env subst)
 ORB_EVAL_PROFILE_NAME=$(circleci env subst "${ORB_EVAL_PROFILE_NAME}")
 
-IFS=' '
-set --
-for arg in ${ORB_EVAL_ARGUMENTS}; do
-    set -- "$@" "${arg}"
-done
+if [ -n "${ORB_EVAL_ARGUMENTS}" ]; then
+    IFS=' '
+    set --
+    for arg in ${ORB_EVAL_ARGUMENTS}; do
+        set -- "$@" "${arg}"
+    done
+fi
 
-# Print the modified arguments
-printf '%s\n' "$@"
- 
-# if [ -n "${ORB_EVAL_ARGUMENTS}" ]; then
-#     aws s3 sync "${ORB_EVAL_FROM}" "${ORB_EVAL_TO}" --profile "${ORB_EVAL_PROFILE_NAME}" "${ORB_EVAL_ARGUMENTS}"
-#     # set -- "$@" "${ORB_EVAL_ARGUMENTS}"
-#     else
-#     aws s3 sync "${ORB_EVAL_FROM}" "${ORB_EVAL_TO}" --profile "${ORB_EVAL_PROFILE_NAME}"
-# fi
-
-# echo "${ORB_EVAL_ARGUMENTS}"
 aws s3 sync "${ORB_EVAL_FROM}" "${ORB_EVAL_TO}" --profile "${ORB_EVAL_PROFILE_NAME}" "$@"
 set +x
