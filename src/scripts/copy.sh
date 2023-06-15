aec5dd96f80f806e06ddeb1764098fb3473665ff
Brian Vu <emailbrian.vu@gmail.com>
2023-06-15 11:45:37 -0700

    test: fancy expansion

--- src/scripts/copy.sh
+++ src/scripts/copy.sh
@@ -1,20 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 ORB_EVAL_FROM=$(circleci env subst "${ORB_EVAL_FROM}")
 ORB_EVAL_TO=$(circleci env subst "${ORB_EVAL_TO}")
 ORB_EVAL_ARGUMENTS=$(echo "${ORB_EVAL_ARGUMENTS}" | circleci env subst)
-
-ORB_EVAL_ARGUMENTS=${ORB_EVAL_ARGUMENTS#\'}
-ORB_EVAL_ARGUMENTS=${ORB_EVAL_ARGUMENTS%\'}
-
 ORB_EVAL_PROFILE_NAME=$(circleci env subst "${ORB_EVAL_PROFILE_NAME}")
 
-if [ -n "${ORB_EVAL_ARGUMENTS}" ]; then
-    aws s3 sync "${ORB_EVAL_FROM}" "${ORB_EVAL_TO}" --profile "${ORB_EVAL_PROFILE_NAME}" "${ORB_EVAL_ARGUMENTS}"
-    # set -- "$@" "${ORB_EVAL_ARGUMENTS}"
-    else
-    aws s3 sync "${ORB_EVAL_FROM}" "${ORB_EVAL_TO}" --profile "${ORB_EVAL_PROFILE_NAME}"
-fi
+# if [ -n "${ORB_EVAL_ARGUMENTS}" ]; then
+#     aws s3 sync "${ORB_EVAL_FROM}" "${ORB_EVAL_TO}" --profile "${ORB_EVAL_PROFILE_NAME}" "${ORB_EVAL_ARGUMENTS}"
+#     # set -- "$@" "${ORB_EVAL_ARGUMENTS}"
+#     else
+#     aws s3 sync "${ORB_EVAL_FROM}" "${ORB_EVAL_TO}" --profile "${ORB_EVAL_PROFILE_NAME}"
+# fi
 
 # set -x
 # aws s3 cp "${ORB_EVAL_FROM}" "${ORB_EVAL_TO}" --profile "${ORB_EVAL_PROFILE_NAME}" "$@"
+aws s3 sync "${ORB_EVAL_FROM}" "${ORB_EVAL_TO}" --profile "${ORB_EVAL_PROFILE_NAME}" "${ORB_EVAL_ARGUMENTS:+$ORB_EVAL_ARGUMENTS}"
 set +x
--- src/scripts/sync.sh
+++ src/scripts/sync.sh
@@ -1,16 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 set -x
 ORB_EVAL_FROM=$(circleci env subst "${ORB_EVAL_FROM}")
 ORB_EVAL_TO=$(circleci env subst "${ORB_EVAL_TO}")
 ORB_EVAL_ARGUMENTS=$(echo "${ORB_EVAL_ARGUMENTS}" | circleci env subst)
 ORB_EVAL_PROFILE_NAME=$(circleci env subst "${ORB_EVAL_PROFILE_NAME}")
 
-if [ -n "${ORB_EVAL_ARGUMENTS}" ]; then
-    aws s3 sync "${ORB_EVAL_FROM}" "${ORB_EVAL_TO}" --profile "${ORB_EVAL_PROFILE_NAME}" "${ORB_EVAL_ARGUMENTS}"
-    # set -- "$@" "${ORB_EVAL_ARGUMENTS}"
-    else
-    aws s3 sync "${ORB_EVAL_FROM}" "${ORB_EVAL_TO}" --profile "${ORB_EVAL_PROFILE_NAME}"
-fi
+# if [ -n "${ORB_EVAL_ARGUMENTS}" ]; then
+#     aws s3 sync "${ORB_EVAL_FROM}" "${ORB_EVAL_TO}" --profile "${ORB_EVAL_PROFILE_NAME}" "${ORB_EVAL_ARGUMENTS}"
+#     # set -- "$@" "${ORB_EVAL_ARGUMENTS}"
+#     else
+#     aws s3 sync "${ORB_EVAL_FROM}" "${ORB_EVAL_TO}" --profile "${ORB_EVAL_PROFILE_NAME}"
+# fi
 
 # echo "${ORB_EVAL_ARGUMENTS}"
+aws s3 sync "${ORB_EVAL_FROM}" "${ORB_EVAL_TO}" --profile "${ORB_EVAL_PROFILE_NAME}" "${ORB_EVAL_ARGUMENTS:+$ORB_EVAL_ARGUMENTS}"
 set +x
