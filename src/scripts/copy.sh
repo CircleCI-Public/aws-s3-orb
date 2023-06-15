7641f058f595e18f7d8471c7300d18bd1d82a796
Brian Vu <emailbrian.vu@gmail.com>
2023-06-14 22:48:13 -0700

    test: separate with while loop

--- src/scripts/copy.sh
+++ src/scripts/copy.sh
@@ -4,8 +4,13 @@ ORB_EVAL_TO=$(circleci env subst "${ORB_EVAL_TO}")
 ORB_EVAL_ARGUMENTS=$(circleci env subst "${ORB_EVAL_ARGUMENTS}")
 ORB_EVAL_PROFILE_NAME=$(circleci env subst "${ORB_EVAL_PROFILE_NAME}")
 
-if [ -z "${ORB_EVAL_ARGUMENTS}" ]; then
-    set -- "$@" "${ORB_EVAL_ARGUMENTS}"
+if [ -n "${ORB_EVAL_ARGUMENTS}" ]; then
+    set -- "${ORB_EVAL_ARGUMENTS}"
+    while [ $# -gt 0 ]; do
+        arg="$1"
+        ser -- "$@" "${arg}"
+        shift
+    done
 fi
 
 set -x
