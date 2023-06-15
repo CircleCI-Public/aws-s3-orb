47fec6519daf80014e790468f98f8af2bacb822f
Brian Vu <emailbrian.vu@gmail.com>
2023-06-14 23:40:48 -0700

    test: tr -d

--- src/scripts/sync.sh
+++ src/scripts/sync.sh
@@ -3,24 +3,13 @@ set -x
 ORB_EVAL_FROM=$(circleci env subst "${ORB_EVAL_FROM}")
 ORB_EVAL_TO=$(circleci env subst "${ORB_EVAL_TO}")
 ORB_EVAL_ARGUMENTS=$(echo "${ORB_EVAL_ARGUMENTS}" | circleci env subst)
-echo "${ORB_EVAL_ARGUMENTS}" | sed "s/'//g"
+ORB_EVAL_ARGUMENTS=$(echo "${ORB_EVAL_ARGUMENTS}" | tr -d "'")
 ORB_EVAL_PROFILE_NAME=$(circleci env subst "${ORB_EVAL_PROFILE_NAME}")
 
-# if [ -n "${ORB_EVAL_ARGUMENTS}" ]; then
-#     IFS=' '
-#     set -- "${ORB_EVAL_ARGUMENTS}"
-#     for arg do
-#         set -- "$@" "${arg}"
-#     done
-# fi
-
-ORB_EVAL_ARGUMENTS=$(echo "$ORB_EVAL_ARGUMENTS" | circleci env subst)
-if [ -n "$ORB_EVAL_ARGUMENTS" ]; then
-    printf '%s\n' "$ORB_EVAL_ARGUMENTS" | while IFS= read -r arg; do
-        ARGUMENTS="$ARGUMENTS $arg"
-    done
+if [ -n "${ORB_EVAL_ARGUMENTS}" ]; then
+    set -- "$@" "${ORB_EVAL_ARGUMENTS}"
 fi
 
-echo "${ARGUMENTS}"
+echo "${ORB_EVAL_ARGUMENTS}"
 aws s3 sync "${ORB_EVAL_FROM}" "${ORB_EVAL_TO}" --profile "${ORB_EVAL_PROFILE_NAME}" "$@"
 set +x
