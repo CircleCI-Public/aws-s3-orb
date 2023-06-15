5e69ace37623aaf60810d5d52f44639b0eede660
Brian Vu <emailbrian.vu@gmail.com>
2023-06-14 23:44:49 -0700

    test: grep

--- src/scripts/copy.sh
+++ src/scripts/copy.sh
@@ -2,8 +2,10 @@
 ORB_EVAL_FROM=$(circleci env subst "${ORB_EVAL_FROM}")
 ORB_EVAL_TO=$(circleci env subst "${ORB_EVAL_TO}")
 ORB_EVAL_ARGUMENTS=$(echo "${ORB_EVAL_ARGUMENTS}" | circleci env subst)
-ORB_EVAL_ARGUMENTS=$(echo "${ORB_EVAL_ARGUMENTS}" | tr -d "'")
 ORB_EVAL_PROFILE_NAME=$(circleci env subst "${ORB_EVAL_PROFILE_NAME}")
+
+ORB_EVAL_ARGUMENTS="$(grep "${ORB_EVAL_ARGUMENTS}")"
+echo "${ORB_EVAL_ARGUMENTS}"
 
 if [ -n "${ORB_EVAL_ARGUMENTS}" ]; then
     set -- "$@" "${ORB_EVAL_ARGUMENTS}"
