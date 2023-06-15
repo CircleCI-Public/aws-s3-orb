746a966a18e2e84f9edda980c35b23bdb740da0e
Brian Vu <emailbrian.vu@gmail.com>
2023-06-15 13:04:37 -0700

    test: debugging

--- src/scripts/copy.sh
+++ src/scripts/copy.sh
@@ -2,7 +2,10 @@
 ORB_EVAL_FROM=$(circleci env subst "${ORB_EVAL_FROM}")
 ORB_EVAL_TO=$(circleci env subst "${ORB_EVAL_TO}")
 ORB_EVAL_ARGUMENTS=$(echo "${ORB_EVAL_ARGUMENTS}" | circleci env subst)
+ORB_EVAL_ARGUMENTS=${ORB_EVAL_ARGUMENTS//\'/}
 ORB_EVAL_PROFILE_NAME=$(circleci env subst "${ORB_EVAL_PROFILE_NAME}")
+
+
 
 # if [ -n "${ORB_EVAL_ARGUMENTS}" ]; then
 #     aws s3 sync "${ORB_EVAL_FROM}" "${ORB_EVAL_TO}" --profile "${ORB_EVAL_PROFILE_NAME}" "${ORB_EVAL_ARGUMENTS}"
@@ -13,5 +16,7 @@ ORB_EVAL_PROFILE_NAME=$(circleci env subst "${ORB_EVAL_PROFILE_NAME}")
 
 # set -x
 # aws s3 cp "${ORB_EVAL_FROM}" "${ORB_EVAL_TO}" --profile "${ORB_EVAL_PROFILE_NAME}" "$@"
+
+
 aws s3 sync "${ORB_EVAL_FROM}" "${ORB_EVAL_TO}" --profile "${ORB_EVAL_PROFILE_NAME}" "${ORB_EVAL_ARGUMENTS:+$ORB_EVAL_ARGUMENTS}"
 set +x
