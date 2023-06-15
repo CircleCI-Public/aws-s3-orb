30e414ea457f17aa684b59f0e2dad0471e5fe223
Brian Vu <emailbrian.vu@gmail.com>
2023-06-14 22:37:28 -0700

    test: testing expansion with white spaces

--- src/scripts/sync.sh
+++ src/scripts/sync.sh
@@ -5,7 +5,7 @@ ORB_EVAL_TO=$(circleci env subst "${ORB_EVAL_TO}")
 ORB_EVAL_ARGUMENTS=$(circleci env subst "${ORB_EVAL_ARGUMENTS}")
 ORB_EVAL_PROFILE_NAME=$(circleci env subst "${ORB_EVAL_PROFILE_NAME}")
 
-if [ -n "${ORB_EVAL_ARGUMENTS}" ]; then
+if [ -z "${ORB_EVAL_ARGUMENTS}" ]; then
     set -- "$@" "${ORB_EVAL_ARGUMENTS}"
 fi
 
