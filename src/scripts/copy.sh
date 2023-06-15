d36197f92dc6f0b862517fa46e566acb459a5709
Brian Vu <64455338+brivu@users.noreply.github.com>
2023-06-15 11:02:25 -0700

    feat: v4.0 (#53)
    * chore: update orb versions
    
    * chore: update varaible names
    
    * feat: add default executor
    
    * refactor: replace job image with default executor
    
    * fix: address pr comments
    
    * fix: move set-x

--- src/scripts/copy.sh
+++ src/scripts/copy.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-ORB_EVAL_FROM=$(circleci env subst "${ORB_EVAL_FROM}")
-ORB_EVAL_TO=$(circleci env subst "${ORB_EVAL_TO}")
-ORB_EVAL_ARGUMENTS=$(circleci env subst "${ORB_EVAL_ARGUMENTS}")
-ORB_EVAL_PROFILE_NAME=$(circleci env subst "${ORB_EVAL_PROFILE_NAME}")
+ORB_EVAL_FROM="$(eval echo "${ORB_EVAL_FROM}")"
+ORB_EVAL_TO="$(eval echo "${ORB_EVAL_TO}")"
+ORB_STR_ARGUMENTS="$(echo "${ORB_STR_ARGUMENTS}" | circleci env subst)"
+ORB_STR_PROFILE_NAME="$(circleci env subst "${ORB_STR_PROFILE_NAME}")"
 
-if [ -n "${ORB_EVAL_ARGUMENTS}" ]; then
-    set -- "$@" "${ORB_EVAL_ARGUMENTS}"
+if [ -n "${ORB_STR_ARGUMENTS}" ]; then
+    set -- "$@" "${ORB_STR_ARGUMENTS}"
 fi
 
 set -x
-aws s3 cp "${ORB_EVAL_FROM}" "${ORB_EVAL_TO}" --profile "${ORB_EVAL_PROFILE_NAME}" "$@"
+aws s3 cp "${ORB_EVAL_FROM}" "${ORB_EVAL_TO}" --profile "${ORB_STR_PROFILE_NAME}" "$@"
 set +x
--- src/scripts/sync.sh
+++ src/scripts/sync.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-set -x
-ORB_EVAL_FROM=$(circleci env subst "${ORB_EVAL_FROM}")
-ORB_EVAL_TO=$(circleci env subst "${ORB_EVAL_TO}")
-ORB_EVAL_ARGUMENTS=$(circleci env subst "${ORB_EVAL_ARGUMENTS}")
-ORB_EVAL_PROFILE_NAME=$(circleci env subst "${ORB_EVAL_PROFILE_NAME}")
 
-if [ -n "${ORB_EVAL_ARGUMENTS}" ]; then
-    set -- "$@" "${ORB_EVAL_ARGUMENTS}"
+ORB_EVAL_FROM="$(circleci env subst "${ORB_EVAL_FROM}")"
+ORB_EVAL_TO="$(circleci env subst "${ORB_EVAL_TO}")"
+ORB_STR_ARGUMENTS="$(echo "${ORB_STR_ARGUMENTS}" | circleci env subst)"
+ORB_STR_PROFILE_NAME="$(circleci env subst "${ORB_STR_PROFILE_NAME}")"
+
+if [ -n "${ORB_STR_ARGUMENTS}" ]; then
+    set -- "$@" "${ORB_STR_ARGUMENTS}"
 fi
-
-aws s3 sync "${ORB_EVAL_FROM}" "${ORB_EVAL_TO}" --profile "${ORB_EVAL_PROFILE_NAME}" "$@"
+set -x
+aws s3 sync "${ORB_EVAL_FROM}" "${ORB_EVAL_TO}" --profile "${ORB_STR_PROFILE_NAME}" "$@"
 set +x
