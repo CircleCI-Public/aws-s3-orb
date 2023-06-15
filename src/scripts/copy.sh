64026cb3603cf80e7c71afc37e41f7821cfe32df
Brian Vu <emailbrian.vu@gmail.com>
2023-06-15 16:40:44 -0700

    test: debug

--- src/scripts/copy.sh
+++ src/scripts/copy.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -x
 ORB_EVAL_FROM=$(circleci env subst "${ORB_EVAL_FROM}")
 ORB_EVAL_TO=$(circleci env subst "${ORB_EVAL_TO}")
@@ -6,12 +6,19 @@ ORB_EVAL_ARGUMENTS=$(echo "${ORB_EVAL_ARGUMENTS}" | circleci env subst)
 ORB_EVAL_PROFILE_NAME=$(circleci env subst "${ORB_EVAL_PROFILE_NAME}")
 
 if [ -n "${ORB_EVAL_ARGUMENTS}" ]; then
+    # IFS=' '
+    # set --
+    # for arg in ${ORB_EVAL_ARGUMENTS}; do
+    #     arg=$(echo "$arg" | tr -d "'")
+    #     set -- "$@" "${arg}"
+    # done
     IFS=' '
-    set --
-    for arg in ${ORB_EVAL_ARGUMENTS}; do
-        arg=$(echo "$arg" | tr -d "'")
+    read -ra args <<< "${ORB_EVAL_ARGUMENTS}"
+    echo "${args[@]}"
+    for arg in "${args[@]}"
+    do
         set -- "$@" "${arg}"
-    done
+    done        
 fi
 
 aws s3 cp "${ORB_EVAL_FROM}" "${ORB_EVAL_TO}" --profile "${ORB_STR_PROFILE_NAME}" "$@"
