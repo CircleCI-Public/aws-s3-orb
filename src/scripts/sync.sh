d199b2e9db4b54431732130d0538d6c51b0eadbd
Brian Vu <64455338+brivu@users.noreply.github.com>
2023-06-16 13:39:51 -0700

    fix: arguments parmeter expansion (#54)
    * fix: parse arguments
    
    * test: add argument parameter to ci tests
    
    * refactor: remove unnecessary executors

--- src/scripts/sync.sh
+++ src/scripts/sync.sh
@@ -6,7 +6,11 @@ ORB_STR_ARGUMENTS="$(echo "${ORB_STR_ARGUMENTS}" | circleci env subst)"
 ORB_STR_PROFILE_NAME="$(circleci env subst "${ORB_STR_PROFILE_NAME}")"
 
 if [ -n "${ORB_STR_ARGUMENTS}" ]; then
-    set -- "$@" "${ORB_STR_ARGUMENTS}"
+    IFS=' '
+    set --
+    for arg in $(echo "${ORB_STR_ARGUMENTS}" | sed 's/,[ ]*/,/g'); do
+    set -- "$@" "$arg"
+    done
 fi
 set -x
 aws s3 sync "${ORB_EVAL_FROM}" "${ORB_EVAL_TO}" --profile "${ORB_STR_PROFILE_NAME}" "$@"
