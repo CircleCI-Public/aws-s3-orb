e2546808d6e120728807094e6822b7e3c66b656f
Mateo Arboleda <mateo.arboleda@circleci.com>
2024-10-11 15:15:10 -0500

    fix linting

--- src/scripts/sync.sh
+++ src/scripts/sync.sh
@@ -13,7 +13,7 @@ if [ -n "${ORB_STR_ARGUMENTS}" ]; then
     done
 fi
 if [ -n "${ORB_STR_PROFILE_NAME}" ]; then
-    set -- "$@" --profile ${ORB_STR_PROFILE_NAME}
+    set -- "$@" --profile "${ORB_STR_PROFILE_NAME}"
 fi
 set -x
 aws s3 sync "${ORB_EVAL_FROM}" "${ORB_EVAL_TO}" "$@"
